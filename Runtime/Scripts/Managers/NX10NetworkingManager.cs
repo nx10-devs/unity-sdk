443aeb17b9562a052c99e1a899832e6cf1f05fbd
Jmack91 <jamesmacklin25@gmail.com>
2026-08-05 11:36:12 +0100

    good one

--- Runtime/Scripts/Managers/NX10NetworkingManager.cs
+++ Runtime/Scripts/Managers/NX10NetworkingManager.cs
@@ -1,14 +1,11 @@
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
-using static System.Net.WebRequestMethods;
 
 namespace NX10
 {
